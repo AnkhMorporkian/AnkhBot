ec344d8be928adc833f8d0168c8b8e647891fc1d
Nikola Kovacevic <nikolak@outlook.com>
2013-12-04 19:19:42 +0100

    PEP8 Changes

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -4,6 +4,7 @@ import datetime
 
 from plugin import AnkhBotPlugin
 
+
 class YouTube(AnkhBotPlugin):
 
     name = "YouTube plugin"
@@ -17,20 +18,24 @@ class YouTube(AnkhBotPlugin):
             return
 
         try:
-            r=requests.get("http://gdata.youtube.com/feeds/api/videos/{}?v=2&alt=jsonc".format(video_id))
-            if r.status_code==200:
-                v_json=r.json()
-                v_json=v_json['data']
+            r = requests.get(
+                "http://gdata.youtube.com/feeds/api/videos/{}?v=2&alt=jsonc".format(video_id))
+            if r.status_code == 200:
+                v_json = r.json()
+                v_json = v_json['data']
             else:
                 return
         except:
             return
 
-        response="Title: {} :: Uploader: {} :: Duration: {} :: Views: {} :: Rating: {:.2f}"
-        self.bot.msg(channel,response.format(v_json['title'],v_json['uploader'],self.format_yt_time(v_json['duration']),
-                        v_json['viewCount'],v_json['rating']))
+        response = "Title: {} :: Uploader: {} :: Duration: {} :: Views: {} :: Rating: {:.2f}"
+        self.bot.msg(
+            channel, response.format(
+                v_json['title'], v_json[
+                    'uploader'], self.format_yt_time(v_json['duration']),
+                v_json['viewCount'], v_json['rating']))
 
-    def format_yt_time(self,seconds):
+    def format_yt_time(self, seconds):
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
         return "%d:%02d:%02d" % (h, m, s)
